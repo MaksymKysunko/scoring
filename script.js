f6122f4da1d1a29a33f84b094456c13dc815ae20
Maksym Kysunko <mkisunko@gmail.com>
2024-07-08 13:39:23 +0300

    Refactored language evaluation, making it unified and maintainable

--- script.js
+++ script.js
@@ -4,182 +4,38 @@ const DEFAULT_DEPLOYMENT_WEIGHT = 0.20;
 const DEFAULT_LANGUAGE_FRAMEWORK_WEIGHT = 0.20;
 const DEFAULT_VERSION_CONTROL_WEIGHT = 0.20;
 
-const frameworks = {
-    Python: {
-        monolith: ["Django", "Flask", "Pyramid"],
-        frontend: ["Django", "Flask", "Pyramid", "Tornado", "Web2py"],
-        backend: ["Django", "Flask", "Pyramid", "Tornado", "Web2py"]
+/*
+const gradeDescriptions = {
+    popularity: {
+        A: "Widely used with a large and active community.",
+        B: "Moderately popular with a reasonably active community.",
+        C: "Somewhat popular with a smaller community.",
+        D: "Rarely used with a minimal community.",
+        E: "Almost never used with an inactive or non-existent community."
     },
-    JavaScript: {
-        monolith: ["Express", "Meteor", "Sails.js"],
-        frontend: ["React", "Vue", "Angular", "Ext JS"],
-        backend: ["Node.js", "Express", "Nest"]
+    support: {
+        A: "Excellent official support and active community support channels.",
+        B: "Good official support and active community support channels.",
+        C: "Adequate support with some community activity.",
+        D: "Limited support and minimal community activity.",
+        E: "Poor support and negligible community activity."
     },
-    Java: {
-        monolith: ["Spring", "Struts", "JSF"],
-        backend: ["Spring", "Struts", "Hibernate"]
+    performance: {
+        A: "Known for excellent performance and efficiency.",
+        B: "Generally good performance with minor inefficiencies.",
+        C: "Average performance, suitable for most tasks.",
+        D: "Below-average performance, may have significant inefficiencies.",
+        E: "Poor performance, generally not recommended for performance-critical applications."
     },
-    PHP: {
-        monolith: ["Laravel", "Symfony", "CodeIgniter"],
-        backend: ["Laravel", "Symfony", "CodeIgniter", "Zend", "CakePHP"]
-    },
-    Ruby: {
-        monolith: ["Rails", "Sinatra", "Hanami"],
-        backend: ["Rails", "Sinatra"]
-    },
-    ".Net": {
-        monolith: [".NET Core", "ASP.NET", "Blazor"],
-        backend: [".NET Core", "ASP.NET"]
+    easeOfUse: {
+        A: "Easy to learn and use, even for beginners.",
+        B: "Moderately easy to learn, with some complexities.",
+        C: "Average learning curve, suitable for developers with some experience.",
+        D: "Steep learning curve, may be challenging for beginners.",
+        E: "Very difficult to learn and use, even for experienced developers."
     }
 };
-
-const versions = {
-    Django: {
-        "3.x": { popularity: 5, support: 5 },
-        "2.x": { popularity: 5, support: 5 },
-        "1.11.x": { popularity: 4, support: 4 },
-        "1.10.x": { popularity: 3, support: 3 },
-        "1.9.x and below": { popularity: 2, support: 2 }
-    },
-    Flask: {
-        "2.x": { popularity: 5, support: 5 },
-        "1.x": { popularity: 4, support: 4 },
-    },
-    Pyramid: {
-        "1.10.x": { popularity: 4, support: 4 },
-        "1.9.x and below": { popularity: 3, support: 3 }
-    },
-    Tornado: {
-        "6.x": { popularity: 5, support: 5 },
-        "5.x": { popularity: 4, support: 4 },
-        "4.x": { popularity: 3, support: 3 },
-        "3.x and below": { popularity: 2, support: 2 }
-    },
-    Web2py: {
-        "2.x": { popularity: 4, support: 4 },
-        "1.x": { popularity: 3, support: 3 }
-    },
-    React: {
-        "17.x": { popularity: 5, support: 5 },
-        "16.x": { popularity: 5, support: 5 },
-        "15.x": { popularity: 4, support: 4 },
-        "14.x and below": { popularity: 3, support: 3 }
-    },
-    Vue: {
-        "3.x": { popularity: 5, support: 5 },
-        "2.x": { popularity: 5, support: 5 },
-        "1.x": { popularity: 4, support: 4 }
-    },
-    Angular: {
-        "11.x": { popularity: 5, support: 5 },
-        "10.x": { popularity: 5, support: 5 },
-        "9.x": { popularity: 4, support: 4 },
-        "8.x and below": { popularity: 3, support: 3 }
-    },
-    "Node.js": {
-        "14.x": { popularity: 5, support: 5 },
-        "12.x": { popularity: 5, support: 5 },
-        "10.x": { popularity: 4, support: 4 },
-        "8.x and below": { popularity: 3, support: 3 }
-    },
-    Express: {
-        "4.x": { popularity: 5, support: 5 },
-        "3.x": { popularity: 4, support: 4 },
-        "2.x": { popularity: 3, support: 3 },
-        "1.x": { popularity: 2, support: 2 }
-    },
-    Spring: {
-        "5.x": { popularity: 5, support: 5 },
-        "4.x": { popularity: 5, support: 5 },
-        "3.x": { popularity: 4, support: 4 },
-        "2.x and below": { popularity: 3, support: 3 }
-    },
-    Struts: {
-        "2.x": { popularity: 4, support: 4 },
-        "1.x": { popularity: 3, support: 3 }
-    },
-    Hibernate: {
-        "5.x": { popularity: 5, support: 5 },
-        "4.x": { popularity: 4, support: 4 },
-        "3.x and below": { popularity: 3, support: 3 }
-    },
-    ".NET Core": {
-        "5.x": { popularity: 5, support: 5 },
-        "3.x": { popularity: 5, support: 5 },
-        "2.x": { popularity: 4, support: 4 },
-        "1.x": { popularity: 3, support: 3 }
-    },
-    "ASP.NET": {
-        "4.x": { popularity: 5, support: 5 },
-        "3.x": { popularity: 4, support: 4 },
-        "2.x": { popularity: 3, support: 3 },
-        "1.x": { popularity: 2, support: 2 }
-    },
-    Laravel: {
-        "8.x": { popularity: 5, support: 5 },
-        "7.x": { popularity: 5, support: 5 },
-        "6.x": { popularity: 4, support: 4 },
-        "5.x and below": { popularity: 3, support: 3 }
-    },
-    Symfony: {
-        "5.x": { popularity: 5, support: 5 },
-        "4.x": { popularity: 5, support: 5 },
-        "3.x": { popularity: 4, support: 4 },
-        "2.x and below": { popularity: 3, support: 3 }
-    },
-    CodeIgniter: {
-        "4.x": { popularity: 4, support: 4 },
-        "3.x": { popularity: 4, support: 4 },
-        "2.x and below": { popularity: 3, support: 3 }
-    },
-    Zend: {
-        "3.x": { popularity: 4, support: 4 },
-        "2.x": { popularity: 4, support: 4 },
-        "1.x": { popularity: 3, support: 3 }
-    },
-    CakePHP: {
-        "4.x": { popularity: 4, support: 4 },
-        "3.x": { popularity: 4, support: 4 },
-        "2.x and below": { popularity: 3, support: 3 }
-    },
-    Rails: {
-        "6.x": { popularity: 5, support: 5 },
-        "5.x": { popularity: 5, support: 5 },
-        "4.x": { popularity: 4, support: 4 },
-        "3.x and below": { popularity: 3, support: 3 }
-    },
-    Sinatra: {
-        "2.x": { popularity: 4, support: 4 },
-        "1.x": { popularity: 3, support: 3 }
-    },
-    Meteor: {
-        "2.x": { popularity: 5, support: 5 },
-        "1.x": { popularity: 4, support: 4 }
-    },
-    "Sails.js": {
-        "1.x": { popularity: 5, support: 5 },
-        "0.x": { popularity: 4, support: 4 }
-    },
-    JSF: {
-        "2.x": { popularity: 4, support: 4 },
-        "1.x": { popularity: 3, support: 3 }
-    },
-    Hanami: {
-        "1.x": { popularity: 4, support: 4 }
-    },
-    "Ext JS": {
-        "7.x": { popularity: 5, support: 5 },
-        "6.x": { popularity: 5, support: 5 },
-        "5.x": { popularity: 4, support: 4 },
-        "4.x and below": { popularity: 3, support: 3 }
-    },
-    Nest: {
-        "7.x": { popularity: 5, support: 5 },
-        "6.x": { popularity: 5, support: 5 },
-        "5.x": { popularity: 4, support: 4 },
-        "4.x and below": { popularity: 3, support: 3 }
-    }
-};
+*/
 
 document.addEventListener('DOMContentLoaded', function() {
     // setting score values as default for input fields
@@ -197,52 +53,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('versionControlWeightDisplay').textContent = DEFAULT_VERSION_CONTROL_WEIGHT.toFixed(2);
 });
 
-function updateComponents() {
-    const language = document.getElementById('language').value;
-    const componentSelect = document.getElementById('component');
-    componentSelect.innerHTML = '<option value="0">Select</option>';
-    if (language !== "0") {
-        for (const component in frameworks[language]) {
-            componentSelect.innerHTML += `<option value="${component}">${component}</option>`;
-        }
-    }
-}
-
-function updateFrameworks() {
-    const language = document.getElementById('language').value;
-    const component = document.getElementById('component').value;
-    const frameworkSelect = document.getElementById('framework');
-    frameworkSelect.innerHTML = '<option value="0">Select</option>';
-    if (language !== "0" && component !== "0") {
-        frameworks[language][component].forEach(framework => {
-            frameworkSelect.innerHTML += `<option value="${framework}">${framework}</option>`;
-        });
-    }
-}
-
-function updateVersions() {
-    const framework = document.getElementById('framework').value;
-    const versionSelect = document.getElementById('version');
-    versionSelect.innerHTML = '<option value="0">Select</option>';
-    if (framework !== "0") {
-        for (const version in versions[framework]) {
-            versionSelect.innerHTML += `<option value="${version}">${version}</option>`;
-        }
-    }
-}
-
-function showScores() {
-    const framework = document.getElementById('framework').value;
-    const version = document.getElementById('version').value;
-    if (framework !== "0" && version !== "0") {
-        const popularity = versions[framework][version].popularity;
-        const support = versions[framework][version].support;
-        document.getElementById('popularity').value = popularity;
-        document.getElementById('communitySupport').value = support;
-    }
-    calculateTotalScore();
-}
-
 function getGrade(score) {
     if (score <= 2) return 'E';
     if (score <= 3) return 'D';
@@ -256,7 +66,7 @@ function calculateTotalScore() {
         codeQuality: ["securityScore","reliabilityScore","MaintanabilityScore", "securityHotspotsScore", "testCoverage",  "commentsDensity"],
         buildCI: ["ciPipeline", 'buildState'],
         deployment: ["deploymentScore"],
-        languageFramework: ["popularity", "communitySupport"],
+        languageFramework: ["popularity", "support", "performance", "easeOfUse"],
         versionControl: ["branchingStrategy", "commitMessages", "pullRequests"]
     };
 
@@ -300,7 +110,7 @@ function calculateTotalScore() {
 
     }
 
-    document.getElementById('totalScoreOnTop').textContent = getGrade(totalWeightedScore);
+    document.getElementById('gradeOnTop').textContent = getGrade(totalWeightedScore);
 
     // Update total scores
     document.getElementById('totalWeight').textContent = totalWeight.toFixed(2);
@@ -308,11 +118,7 @@ function calculateTotalScore() {
 }
 
 // Export functions for use in HTML
-/* exported updateComponents */
-window.updateComponents = updateComponents;
-/* exported updateFrameworks */
-window.updateFrameworks = updateFrameworks;
-/* exported pdateVersions*/
-window.updateVersions = updateVersions;
-/* exported showScores */
-window.showScores = showScores;
+/* exported updateLanguageEvaluation */
+window.updateLanguageEvaluation = updateLanguageEvaluation;
+/* exported calculateTotalScore */
+window.calculateTotalScore = calculateTotalScore;
